531cfd874e337846675087ebad6c7856928b0a77
Omnyamohamed22393 <omnyamohammed642@gmail.com>
2025-07-05 04:04:50 +0300

    Edit routes to use Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
-import { MovieList } from './components/movie-list/movie-list';
-import { MovieDetails } from './components/movie-details/movie-details';
-import { SearchResults } from './components/search-results/search-results';
-import { Wishlist } from './components/wishlist/wishlist';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'movies', pathMatch: 'full'},
-  { path: 'movies', component: MovieList, data: { mediaType: 'movie', title: 'Now Playing Movies' }},
-  { path: 'tvshows', component: MovieList, data: { mediaType: 'tv', title: 'Popular TV Shows' } },
-  { path: 'details/:id/:type', component: MovieDetails},
-  { path: 'search', component: SearchResults},
-  { path: 'wishlist', component: Wishlist },
+  { path: 'movies', loadComponent: () => import('./components/movie-list/movie-list').then(m => m.MovieList), data: { mediaType: 'movie', title: 'Now Playing Movies' }},
+  { path: 'tvshows', loadComponent: () => import('./components/movie-list/movie-list').then(m => m.MovieList), data: { mediaType: 'tv', title: 'Popular TV Shows' } },
+  { path: 'details/:id/:type', loadComponent: () => import('./components/movie-details/movie-details').then(m => m.MovieDetails)},
+  { path: 'search', loadComponent: () => import('./components/search-results/search-results').then(m => m.SearchResults)},
+  { path: 'wishlist', loadComponent: () => import('./components/wishlist/wishlist').then(m => m.Wishlist)},
 
   { path: '**', redirectTo: '' }
 ];
